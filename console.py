54a754ceb13f1d431f0f644448b6081e5f8806e7
JuliusDgenius <ibejulius1@gmail.com>
2022-08-08 03:21:47 +0100

    tasks

--- console.py
+++ console.py
@@ -1,16 +1,51 @@
 #!/usr/bin/python3
+"""Defines the HBNB console"""
 import cmd
-import models
 import re
 from shlex import split
-# from models import storage
+from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        return [i.strip(",") for i in split(arg)]
+    else:
+        lexer = slpit(arg[:brackets.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(brackets.group())
+        return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBNB interpreter
+    Attributes:
+        prompt (str): The command prompt.
+    """
 
     prompt = '(hbnb) '
-    __classes = ['BaseModel']
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     def __init__(self, completekey='tab', stdin=None, stdout=None):
         """init method"""
@@ -19,6 +54,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """do nothing"""
         pass
+
+    def default(self, arg):
+        """Default behaviour of cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+    }
+    match = re.search(r"\.", arg)
+    if match is not None:
+        arg1 = [arg[:match.span()[0]], arg[match.span()[1]:]]
+        match = re.search(r"\((.*?)\)", arg1[1])
+        if match is not None:
+            command = [arg1[1][:match.span()[0]], match.group()[1:-1]]
+            if command[0] in argdict.keys():
+                call = "{} {}".format(arg1[0], command[1])
+                return argdict[command[0]](call)
+    print("*** Unknown syntax: {}".format(arg))
+    return False
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -106,6 +162,16 @@ class HBNBCommand(cmd.Cmd):
                 print(resp)
             except Exception as e:
                 print(e)
+
+    def do_count(self, arg):
+        """Usage: count <class> or <class>.count()
+        Retrieve the number of instances of given class"""
+        arg1 = parse(arg)
+        count = 0
+        for obj in storage.all().values():
+            if arg1[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
 
     def do_update(self, arg):
         """
