b009b08ca53ccb239462d905e07c7f58f343b2bd
JuliusDgenius <ibejulius1@gmail.com>
2022-08-09 10:30:55 +0100

    corrections

--- models/base_model.py
+++ models/base_model.py
@@ -1,53 +1,66 @@
-#!usr/bin/python3
-"""Defines the BaseModel class."""
-import models
+#!/usr/bin/python3
+"""
+base class file
+"""
+
 import uuid
 from datetime import datetime
+import models
+from json import JSONEncoder
 
 
 class BaseModel:
-    """Represent the base model.
-    Represents the "base" for all other classes in AirBnB project.
+    """
+    =========
+    BaseModel
+    =========
     """
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new Base.
-        Args:
-            *args: as many arguments.
-            **kwargs: key/pair value arguments.
-        """
-        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+        """initialize the instance of the class"""
         if kwargs:
-            for k, v in kwargs.items():
-                if k == "__class__":
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(
+                            value,
+                            '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "__class__":
                     continue
-                elif k == "created_at" or k == "updated_at":
-                    time = datetime.strptime(v, tformat)
-                    setattr(self, k, time)
-                else:
-                    setattr(self, k, v)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
 
-    def __str__(self):
-        """return the print/str representation of the BaseModel instance"""
-        clsname = self.__class__.__name__
-        return ("[{}] ({}) {}".format(clsname, self.id, self.__dict__))
+                setattr(self, key, value)
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                models.storage.new(self)
 
     def save(self):
-        """updates the current datetime after changes"""
+        """save new informations to the class object"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dict containing all keys/values"""
-        dict_rep = {}
-        for k, v in self.__dict__.items():
-            dict_rep[k] = v
-            if isinstance(v, datetime):
-                dict_rep[k] = v.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        dict_rep["__class__"] = type(self).__name__
-        return dict_rep
+        """return dictionary representaton of the instance"""
+        dict_repr = {}
+        for key, value in self.__dict__.items():
+            dict_repr[key] = value
+            if isinstance(value, datetime):
+                dict_repr[key] = value.strftime('%Y-%m-%dT%H:%M:%S.%f')
+            dict_repr["__class__"] = type(self).__name__
+            return dict_repr
+
+    def __str__(self):
+        """return the string formated message when instance is called"""
+        clName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clName, self.id, self.__dict__)
+
+
+class BaseModelEncoder(JSONEncoder):
+    """JSON Encoder for BaseModel
+    """
+
+    def default(self, o):
+        """ default"""
+        if isinstance(o, BaseModel):
+            return o.to_dict()
+        return super().default(o)
