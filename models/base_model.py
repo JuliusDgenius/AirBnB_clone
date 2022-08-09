d2ca15519299f09b2845f9e3da1e71721186ec10
JuliusDgenius <ibejulius1@gmail.com>
2022-08-09 10:50:33 +0100

    corrections

--- models/base_model.py
+++ models/base_model.py
@@ -11,26 +11,26 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-    """Initialize a new Base.
-    Args:
-        *args: as many arguments.
-        **kwargs: key/pair value arguments.
-    """
-    tformat = "%Y-%m-%dT%H:%M:%S.%f"
-    if kwargs:
-        for k, v in kwargs.items():
-            if k == "__class__":
-                continue
-            elif k == "created_at" or k == "updated_at":
-                time = datetime.strptime(v, tformat)
-                setattr(self, k, time)
-            else:
-                setattr(self, k, v)
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
-                models.storage.new(self)
+        """Initialize a new Base.
+        Args:
+            *args: as many arguments.
+            **kwargs: key/pair value arguments.
+        """
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "__class__":
+                    continue
+                elif k == "created_at" or k == "updated_at":
+                    time = datetime.strptime(v, tformat)
+                    setattr(self, k, time)
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """return the print/str representation of the BaseModel instance"""
