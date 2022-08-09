302c848853653c4117db32745f82f8dd9a6ef41d
JuliusDgenius <ibejulius1@gmail.com>
2022-08-09 07:15:12 +0100

    corrected indentation error

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -22,21 +22,21 @@ class TestBaseModelDocs(unittest.TestCase):
 #    def test_pep8_approval(self):
 #       """Tests that models/base_model.py conforms to PEP8 standards"""
 #        for path in ['models/base_models.py'
- #                    'tests/test_models/test_base_model.py']:
+#            'tests/test_models/test_base_model.py']:
 #          with self.subTest(path=path):
-#               errors = pycodestyle.Checker(path).checkall()
- #               self.assertEqual(errors, 0)
+#              errors = pycodestyle.Checker(path).checkall()
+#              self.assertEqual(errors, 0)
 
     def test_module_docstring(self):
         """Test for existence of module docstring"""
         self.assertIsNot(mdoc, None,
-                          "base_model.py has no docstring")
+                         "base_model.py has no docstring")
         self.assertTrue(len(mdoc) > 1,
-                            "base_model.py has no docstring")
+                        "base_model.py has no docstring")
 
     def test_class_docstring(self):
         """Test for the BaseModel class docstring"""
         self.assertIsNot(BaseModel.__doc__, None,
-                            "BaseModel class needs a docstring")
+                         "BaseModel class needs a docstring")
         self.assertTrue(len(BaseModel.__doc__) >= 1,
                         "BaseModel class needs a docstring")
