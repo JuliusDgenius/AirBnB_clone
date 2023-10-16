39b8df1537255bbd697d72bb72fe0a33dc21db29
JuliusDgenius <ibejulius1@gmail.com>
2023-10-16 23:31:17 +0100

    testcases for review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -4,22 +4,49 @@ Test file for user class
 """
 
 import unittest
+from datetime import datetime
+import time
 from models.review import Review
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
 from models.base_model import BaseModel
 
 
-class TestClass(unittest.TestCase):
-    """Test cases"""
+class TestReview(unittest.TestCase):
+    """Test Cases for the Review class."""
 
-    def test_create_instance(self):
-        """create a new instance"""
-        new_state = Review()
-        self.assertIsInstance(new_state, Review)
+    def setUp(self):
+        """Sets up test methods."""
+        pass
 
-    def test_create_instance2(self):
-        """create a new instance"""
-        new_state = Review()
-        self.assertIsInstance(new_state, BaseModel)
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of Review class."""
+        b = Review()
+        self.assertEqual(str(type(b)), "<class 'models.review.Review'>")
+        self.assertIsInstance(b, Review)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of Review class."""
+        attributes = storage.attributes()["Review"]
+        o = Review()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
 
 
 if __name__ == '__main__':
