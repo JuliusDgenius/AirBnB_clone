495493533843db4299ff286c321936add073434a
JuliusDgenius <ibejulius1@gmail.com>
2023-10-16 23:27:25 +0100

    testcases for place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,24 +2,50 @@
 """
 Test file for user class
 """
-
 import unittest
+from datetime import datetime
+import time
 from models.place import Place
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
 from models.base_model import BaseModel
 
 
-class TestClass(unittest.TestCase):
-    """Test cases"""
+class TestPlace(unittest.TestCase):
+    """Test Cases for the Place class."""
 
-    def test_create_instance(self):
-        """create a new instance"""
-        new_place = Place()
-        self.assertIsInstance(new_place, Place)
+    def setUp(self):
+        """Sets up test methods."""
+        pass
 
-    def test_create_instance2(self):
-        """create a new instance"""
-        new_place = Place()
-        self.assertIsInstance(new_place, BaseModel)
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of Place class."""
+        b = Place()
+        self.assertEqual(str(type(b)), "<class 'models.place.Place'>")
+        self.assertIsInstance(b, Place)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of Place class."""
+        attributes = storage.attributes()["Place"]
+        o = Place()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
 
 
 if __name__ == '__main__':
