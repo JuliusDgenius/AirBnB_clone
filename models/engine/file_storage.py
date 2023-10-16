aa4602a4c676df797f827746134dcbeff2d8b4e1
JuliusDgenius <ibejulius1@gmail.com>
2023-10-16 23:52:27 +0100

    correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,7 +32,7 @@ class FileStorage:
         """converts object to JSON and store in file"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             dct = {key: value.to_dict() for key, value in
-                      FileStorage.__objects.items()}
+                   FileStorage.__objects.items()}
             json.dump(dct, f)
 
     def classes(self):
@@ -63,7 +63,7 @@ class FileStorage:
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             ob_dict = json.load(f)
             ob_dict = {key: self.classes()[val["__class__"]](**val)
-                        for key, val in ob_dict.items()}
+                       for key, val in ob_dict.items()}
             FileStorage.__objects = ob_dict
 
     def update(self, obj_name, obj_id, attr, value):
@@ -78,3 +78,37 @@ class FileStorage:
     def delete(self, obj_name, obj_id):
         """deletes object with id 'obj_id"""
         return self.__objects.pop("{}.{}".format(obj_name, obj_id))
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+                "BaseModel":
+                {"id": str,
+                    "created_at": datetime.datetime,
+                    "updated_at": datetime.datetime},
+                "User":
+                {"email": str,
+                    "password": str,
+                    "first_name": str,
+                    "last_name": str},
+                "State": {"name": str},
+                "City": {"state_id": str,
+                         "name": str},
+                "Amenity": {"name": str},
+                "Place": {"city_id": str,
+                          "user_id": str,
+                          "name": str,
+                          "description": str,
+                          "number_rooms": int,
+                          "number_bathrooms": int,
+                          "max_guest": int,
+                          "price_by_night": int,
+                          "latitude": float,
+                          "longitude": float,
+                          "amenity_ids": list},
+                "Review": {"place_id": str,
+                           "user_id": str,
+                           "text": str}
+                }
+
+        return attributes
